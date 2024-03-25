4c6c671768a0844d61e3ffe66aaf8a63e2200750
Emundyt0 <164874815+Emundyt0@users.noreply.github.com>
2024-03-25 23:42:17 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-REPO="https://raw.githubusercontent.com/Emundyt0/Auto-Sc/main/"
+REPO="https://raw.githubusercontent.com/Emundyt0/Auto-ScVPS/main/"
 red='\e[1;31m'
 green='\e[1;32m'
 yell='\e[1;33m'
